45addb559aaca000be5c94bb95d52531f31c2b2d
keri van der Westhuizen <keri.vdw@gmail.com>
2023-04-25 13:47:20 +1200

    update database config

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -30,10 +30,14 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    connection: {
-      filename: '/app/storage/dev.sqlite3',
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
     },
-    useNullAsDefault: true,
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 }
